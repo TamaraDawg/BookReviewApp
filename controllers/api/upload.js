e85a161fbe1291cfde66a5041cfbbedf4538939b
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-05-30 19:33:20 +1000

    added comments

--- controllers/api/upload.js
+++ controllers/api/upload.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const multer = require("multer");
 
-const { checkFileType, upload, storageEngine} = require('../../utils/imgValidation.js');
+const { checkFileType, upload, storageEngine} = require('../../utils/imgValidation.js'); // import the multer configuration from utils
 
 const path = require("path");
 
@@ -13,11 +13,11 @@ router.get('/', (req, res) => {
 
 router.post("/", upload.single("image"), async (req, res) => {
   try {
-
+    // upload image route
     const file = req.file;
     console.log(file);
 
-    if (!file) {
+    if (!file) { // makes sure there is a file
       res.status(400).send("Please upload a file");
     } 
 
--- utils/imgValidation.js
+++ utils/imgValidation.js
@@ -6,27 +6,27 @@ const checkFileType = function (file, cb) {
   const fileTypes = /jpeg|jpg|png/;
 
   //check extension names
-  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase()); // checks the extension name of the file and returns true if it matches the fileTypes variable
 
-  const mimeType = fileTypes.test(file.mimetype);
+  const mimeType = fileTypes.test(file.mimetype); 
 
-  if (mimeType && extName) {
+  if (mimeType && extName) { // if the file is an image then return true else return error... is what I think this is doing
     return cb(null, true);
   } else {
     cb("Error: You can Only Upload Images!!");
   }
 };
 
-const storageEngine = multer.diskStorage({
+const storageEngine = multer.diskStorage({ // location and name of the file
   destination: path.resolve(__dirname, '../public/uploads'),
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}--${file.originalname}`);
+    cb(null, `${Date.now()}--${file.originalname}`); // file name will be the date and time the file was uploaded
   },
 });
 
 const upload = multer({ 
   storage: storageEngine,
-  limits: { fileSize: 10000000 },
+  limits: { fileSize: 10000000 }, // file size limit
   fileFilter: (req, file, cb) => {
     checkFileType(file, cb);
   },
